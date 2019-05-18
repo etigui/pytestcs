7e6b8a765d9d5954746a88263d9f1a61a1a7cd2c
etigui <32940684+etigui@users.noreply.github.com>
2019-05-18 21:35:17 +0200

    Update example

--- example/apaf/test_useless.py
+++ example/apaf/test_useless.py
@@ -2,4 +2,4 @@ import pytest
 import useless
 
 def test_add(a, b, expected):
-    assert useless.add(int(a), int(b)) == int(expected)
+    assert useless.add(int(a), int(b)) == int(expected), "Add 2 number from list"
